8713499b14081b4af2e2d8e839c31fdd22ec3c86
aallymbekova <alymbekovaaliya@icloud.com>
2025-08-22 21:24:21 +0600

    fixed

--- src/test/java/ProductsDetailsTest.java
+++ src/test/java/ProductsDetailsTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static io.qameta.allure.Allure.step;
 
+@Tag(Tags.SMOKE)
+@Tag(Tags.WEB)
 public class ProductsDetailsTest extends BaseWebTest {
 
     @Test
