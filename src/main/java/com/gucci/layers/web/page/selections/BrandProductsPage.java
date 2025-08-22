a9a2a7ce4e1f92a066bc3aa235f1b1705d95b970
Kaniet <mukaev.kaniet@gmail.com>
2025-08-22 19:26:06 +0600

    test cases 16-20 are done

---
+++ src/main/java/com/gucci/layers/web/page/selections/BrandProductsPage.java
@@ -0,0 +1,26 @@
+package com.gucci.layers.web.page.selections;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.gucci.layers.web.page.BasePage;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.page;
+
+public class BrandProductsPage extends BasePage<BrandProductsPage> {
+    public SelenideElement brandProductsHeader = $x("//h2[@class='title text-center']");
+
+    @Override
+    public BrandProductsPage waitForPageLoaded() {
+        brandProductsHeader.shouldBe(Condition.visible);
+        return this;
+    }
+
+    @Step("Click on any brand name")
+    public BrandProductsPage clickBrandByName(String brandName) {
+        SelenideElement brand = $x("//a[text()='" + brandName + "']");
+        elementManager.click(brand);
+        return page(BrandProductsPage.class);
+    }
+}
--- src/test/java/PlaceOrderTest.java
+++ src/test/java/PlaceOrderTest.java
@@ -1,14 +1,22 @@
 
+import com.gucci.data.Sections;
 import com.gucci.entities.User;
 import com.gucci.entities.UserGenerated;
 import com.gucci.layers.web.page.home.HomePage;
+import com.gucci.layers.web.page.selections.CartPage;
+import com.gucci.layers.web.page.selections.ProductsPage;
 import com.gucci.layers.web.page.signup_login.LoginPage;
 import com.gucci.layers.web.page.signup_login.SignUpPage;
 import io.qameta.allure.Owner;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import static com.gucci.data.Sections.SIGN_IN_LOGIN;
+
+import java.util.List;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+import static com.gucci.data.Sections.*;
 
 public class PlaceOrderTest extends BaseWebTest {
 
@@ -44,7 +52,7 @@ public class PlaceOrderTest extends BaseWebTest {
                 .clickPlaceOrder()
                 .fillPaymentOrder()
                 .clickPayAndConfirmOrder()
-                .verifyCongratulationsYourOrderHasBeenConfirmed()
+                .verifyOrderPlacedSuccessfully()
                 .clickDeleteAccount()
                 .waitForPageLoaded();
     }
@@ -77,8 +85,165 @@ public class PlaceOrderTest extends BaseWebTest {
                 .clickPlaceOrder()
                 .fillPaymentOrder()
                 .clickPayAndConfirmOrder()
-                .verifyCongratulationsYourOrderHasBeenConfirmed()
+                .verifyOrderPlacedSuccessfully()
                 .clickDeleteAccount()
                 .waitForPageLoaded();
+    }
+
+    @Test
+    @DisplayName("Place Order: Login before Checkout")
+    @Owner("Kaniet")
+    @Tag("Test case: 16")
+    public void placeOrderBeforeCheckoutTest() {
+
+        User firstUser = UserGenerated.randomUser();
+
+        var blue_top = "Blue Top";
+        var men_tShirt = "Men Tshirt";
+
+        open("", HomePage.class)
+                .waitForPageLoaded()
+                .clickSignupLoginBtn()
+                .waitForPageLoaded()
+                .fillName(firstUser.getName())
+                .fillEmail(firstUser.getEmail())
+                .clickSignUpBtn(SignUpPage.class)
+                .waitForPageLoaded()
+                .signUpNewUser(firstUser)
+                .waitForPageLoaded()
+                .clickContinueBtn()
+                .verifyLoggedAsUserNameVisible(firstUser.getName())
+                .addProductToCart(blue_top)
+                .clickContinueShopping()
+                .addProductToCart(men_tShirt)
+                .clickContinueShopping()
+                .switchBetweenSection(CART_PAGE, CartPage.class)
+                .waitForPageLoaded()
+                .clickProceedCheckout()
+                .verifyAddressDetailsText()
+                .verifyReviewYourOrderText()
+                .inputFormControl()
+                .clickPlaceOrder()
+                .fillPaymentOrder()
+                .clickPayAndConfirmOrder()
+                .verifyOrderPlacedSuccessfully()
+                .clickDeleteAccount()
+                .waitForPageLoaded()
+                .clickContinue();
+    }
+
+    @Test
+    @DisplayName("Remove Products From Cart")
+    @Owner("Kaniet")
+    @Tag("Test case: 17")
+    public void removeProductsFromCartTest() {
+
+        var blue_top = "Blue Top";
+        var men_tShirt = "Men Tshirt";
+
+        open("", HomePage.class)
+                .waitForPageLoaded()
+                .addProductToCart(blue_top)
+                .clickContinueShopping()
+                .addProductToCart(men_tShirt)
+                .clickContinueShopping()
+                .switchBetweenSection(CART_PAGE, CartPage.class)
+                .waitForPageLoaded()
+                .clickDeleteProductFromCart()
+                .clickDeleteProductFromCart()
+                .verifyThatProductIsRemovedFromTheCart();
+    }
+
+    @Test
+    @DisplayName("View Category Products")
+    @Owner("Kaniet")
+    @Tag("Test case: 18")
+    public void viewCategoryProductsTest() {
+
+        open("", HomePage.class)
+                .verifyLeftSideBarIsVisible()
+                .clickWomenCategory()
+                .clickWomenSubCategory()
+                .waitForPageLoaded()
+                .clickMenCategoryAndSubCategory()
+                .verifyMenTShirtsProductsHeaderIsVisible();
+    }
+
+    @Test
+    @DisplayName("View & Cart Brand Products")
+    @Owner("Kaniet")
+    @Tag("Test case: 19")
+    public void viewAndCartBrandsProductsTest() {
+
+        open("products", ProductsPage.class)
+                .verifyBrandsOnLeftSideBarAreVisible()
+                .waitForPageLoaded()
+                .clickBrandByName("Polo")
+                .waitForPageLoaded()
+                .clickBrandByName("H&M")
+                .waitForPageLoaded();
+    }
+
+    @Test
+    @DisplayName("Search Products and Verify Cart After Login")
+    @Owner("Kaniet")
+    @Tag("Test case: 20")
+    public void searchProductsAndVerifyCartAfterLoginTest() {
+
+        User firstUser = UserGenerated.randomUser();
+        SoftAssertions softly = new SoftAssertions();
+
+        var email = firstUser.getEmail();
+        var password = firstUser.getPassword();
+
+        var blue_top = "Blue Top";
+        var men_tShirt = "Men Tshirt";
+
+        open("", HomePage.class)
+                .waitForPageLoaded()
+                .clickSignupLoginBtn()
+                .waitForPageLoaded()
+                .fillName(firstUser.getName())
+                .fillEmail(firstUser.getEmail())
+                .clickSignUpBtn(SignUpPage.class)
+                .waitForPageLoaded()
+                .signUpNewUser(firstUser)
+                .waitForPageLoaded()
+                .clickContinueBtn()
+                .verifyLoggedAsUserNameVisible(firstUser.getName())
+                .clickLogout();
+
+        var cartPage =  open("", HomePage.class)
+                .switchBetweenSection(PRODUCT_PAGE, ProductsPage.class)
+                .waitForPageLoaded()
+                .fillSearchInput(blue_top)
+                .clickSearchBtn()
+                .verifySearchedProductsHeaderIsVisible()
+                .verifyAllSearchedProductsAreVisible(blue_top)
+                .addProductToCart(blue_top)
+                .clickViewCart();
+
+        step("Проверка выбранных продуктов в корзине", () -> {
+            List<String> expectedProducts = List.of(blue_top);
+            softly.assertThat(cartPage.verifyProductsInCart())
+                    .as("Verify products in cart")
+                    .isEqualTo(expectedProducts);
+        });
+
+        open("", HomePage.class)
+                .waitForPageLoaded()
+                .clickSignupLoginBtn()
+                .waitForPageLoaded()
+                .loginUser(email, password, HomePage.class)
+                .switchBetweenSection(CART_PAGE, CartPage.class);
+
+        step("Проверка выбранных продуктов в корзине", () -> {
+            List<String> expectedProducts = List.of(blue_top);
+            softly.assertThat(cartPage.verifyProductsInCart())
+                    .as("Verify products in cart")
+                    .isEqualTo(expectedProducts);
+        });
+
+        softly.assertAll();
     }
 }
