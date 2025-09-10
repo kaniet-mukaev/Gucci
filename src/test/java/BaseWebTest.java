9a432e634357f700c3c1fdd5f44d85668952a843
Kaniet <mukaev.kaniet@gmail.com>
2025-09-10 18:08:49 +0600

    chore: update config/driver and BaseWebTest

--- src/test/java/BaseWebTest.java
+++ src/test/java/BaseWebTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.gucci.layers.web.manager.WebDriverManager;
+import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,8 +13,11 @@ import static com.gucci.config.ConfigurationManager.getAppConfig;
 public class BaseWebTest {
 
     private final String BASE_URL = getAppConfig().baseUrl();
+    @Step("Open the {1}")
     public <T> T open(String endPoint, Class<T> clazz) {
-        return Selenide.open(String.format("%s/%s", BASE_URL, endPoint), clazz);
+        T page = Selenide.open(String.format("%s/%s", BASE_URL, endPoint), clazz);
+        WebDriverManager.configureRemoteDriver();
+        return page;
     }
 
     @BeforeAll
